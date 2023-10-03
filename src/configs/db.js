f274d1f096b13784a05efc929a8560e566aec3a5
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-03 12:59:28 +0530

    Mongodb url changed.

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    return mongoose.connect(`mongodb+srv://khushboo:${process.env.DB_PASS}@cluster0.cfj9f.mongodb.net/product?retryWrites=true&w=majority`)
-}
+  return mongoose.connect(process.env.MONGODB_URL);
+};
 // mongodb+srv://khushboo:<password>@cluster0.cfj9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
