09561c33a7934969d4212f11091971a512f797eb
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-08 21:26:31 +0530

    Display name added and path

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -1,49 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 const newToken = (user) => {
-    return jwt.sign({ user }, process.env.JWT_SECRET_KEY);
-}
+  return jwt.sign({ user }, process.env.JWT_SECRET_KEY);
+};
 
 const signup = async (req, res, next) => {
+  try {
+    // console.log(req.body)
+    let user = await User.findOne({ email: req.body.email }).lean().exec();
 
-    try {
-        // console.log(req.body)
-    let user = await User.findOne({email: req.body.email}).lean().exec();
-
-    if(user) {
-        return res.send('User already exists')
+    if (user) {
+      return res.send("User already exists");
     }
 
     user = await User.create(req.body);
 
     const Token = newToken(user);
-    console.log(user, Token);
-    return res.render("users/login.ejs", {user})
-} catch(err) {
-    return res.send(err.message)
-}
-}
+    // console.log(user, Token);
+    return res.render("users/login.ejs", { user, Token });
+  } catch (err) {
+    return res.send(err.message);
+  }
+};
 
 const login = async (req, res, next) => {
-    try {
-        // console.log(req.body)
-    let user = await User.findOne({email: req.body.email});
-    // console.log(user)
-    if(!user)
-    return res.send('Either user id or password is incorrect');
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.send("Either user id or password is incorrect");
     const match = user.checkPassword(req.body.password);
     if (!match) {
       return res.send({ message: "Either email or password is incorrect" });
     }
-
     const Token = newToken(user);
-     return res.render("users/index.ejs", { Token });
-    } catch(err) {
-        return res.send(err.message)
-    }
-}
+    let displayName = user.first_name + " " + user.last_name;
 
-module.exports = {signup, login}
+    return res.render("users/index.ejs", { Token, displayName });
+  } catch (err) {
+    return res.send(err.message);
+  }
+};
+
+module.exports = { signup, login };
--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -1,12 +1,13 @@
 const express = require("express");
-// const ejs = require('ejs')
 const router = express.Router();
-const Product = require("../models/productapi.modle");
 
 router.get("", async (req, res) => {
   try {
     console.log("Tata Cliq Started");
-    return res.render("users/index.ejs");
+    const displayName = req.user
+      ? req.user.first_name + " " + req.user.last_name
+      : null;
+    return res.render("users/index.ejs", { displayName });
   } catch (err) {
     return res.send("err", err.message);
   }
